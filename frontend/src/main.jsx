5ae4a6a20feae87e20a676a273f6f6ad7d2ba140
AakashJha04 <aakashwillcode@gmail.com>
2026-07-09 22:24:24 +0530

    Added shadcn setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from '@/components/ui/button'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold">Hello World</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <Button>shadcn is working</Button>
+    </div>
   </StrictMode>,
 )
