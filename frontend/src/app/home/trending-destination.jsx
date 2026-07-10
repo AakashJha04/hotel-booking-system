afa2305f6707d728bd2b07d305e6e9f11a3fc464
AakashJha04 <aakashwillcode@gmail.com>
2026-07-10 20:51:59 +0530

    Added footer

--- frontend/src/app/home/trending-destination.jsx
+++ frontend/src/app/home/trending-destination.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { TRENDING_DESTINATIONS } from '@/config/app.config'
+import { cn, getAssetPath } from '@/lib/utils'
 
 const TrendingDestination = () => {
   return (
-    <div>TrendingDestination</div>
+    <div className="container my-16">
+      <div className="mb-4 space-y-1">
+        <h2 className="text-3xl font-bold">Trending Destinations</h2>
+        <p className="text-lg text-muted-foreground">Most popular destinations for travelers</p>
+      </div>
+
+      <div className="grid grid-cols-6 gap-4">
+        {TRENDING_DESTINATIONS.map((destination, index) => (
+          <div key={index}
+            className={cn('h-[270px] rounded-lg overflow-hidden relative', destination.className)}
+          >
+            <img
+              src={getAssetPath(destination.image)}
+              alt={destination.title}
+              className="object-cover size-full rounded-lg"
+            />
+            <div className="absolute from-70% to-100% inset-0 size-full bg-gradient-to-t from-transparent to-blue-800/60 ">
+              <div className='p-3'>
+                <h3 className='text-xl font-bold'>{destination.title}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/layouts/footer.layout.jsx
+++ frontend/src/components/layouts/footer.layout.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { FOOTER_SECTION } from '@/config/app.config'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (<div className='bg-secondary'>
+    <footer className="container py-8">
+      <div className='grid grid-cols-[repeat(auto-fill,_minmax(190px,_1fr))] py-4 gap-6'>
+        {FOOTER_SECTION.map((section, index) => (
+          <div key={index} className='flex flex-col gap-4'>
+            <h3 className='text-sm font-bold'>{section.title}</h3>
+            <ul>
+              {section.links.map((link, linkIndex) => (
+                <li key={linkIndex} >
+                  <a href={link.href} target="_blank" rel="noopener noreferrer">
+                    {link.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </footer>
+  </div>)
 }
 
 export default Footer
