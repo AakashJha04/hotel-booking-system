bdae31573937d4437c69b6ab94d12cbbacc8243f
AakashJha04 <aakashwillcode@gmail.com>
2026-07-10 11:55:35 +0530

    Added header

--- frontend/src/components/layouts/header.layout.jsx
+++ frontend/src/components/layouts/header.layout.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
-import {SERVICE_LIST} from '@/config/app.config'
+import { SERVICE_LIST } from '@/config/app.config'
 import Icon from '../ui/icon'
 
 const Header = () => {
   return (
     <header className="bg-brand py-2">
-      
+
       <div className="container flex items-center justify-between">
-        
+
         <div id="logo-wrapper">
           <a href="#" aria-label="Go to Booking.com">
-            <img width={144} height={24} src="/assets/booking.com.svg" alt="Logo of Booking.com"/>
+            <img width={144} height={24} src="/assets/booking.com.svg" alt="Logo of Booking.com" />
           </a>
         </div>
 
@@ -23,8 +23,9 @@ const Header = () => {
       </div>
 
       <div className="container flex items-center justify-center gap-2 py-2">
-        {SERVICE_LIST.map(item => 
-          <Button>
+        {SERVICE_LIST.map(item =>
+          <Button className={`bg-transparent shadow-none font-normal rounded-full hover:bg-white/10 
+          cursor-pointer flex items-center gap-2 px-6 h-11 ${item.active && 'border border-white bg-white/10'}`} key={item.id}>
             <Icon icon={item.icon} />
             {item.title}
           </Button>
