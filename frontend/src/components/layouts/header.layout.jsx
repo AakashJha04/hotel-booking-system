a93d5cefd87536473a1ebe5841cc81d749871605
AakashJha04 <aakashwillcode@gmail.com>
2026-07-10 10:58:41 +0530

    added assets

--- frontend/src/components/layouts/header.layout.jsx
+++ frontend/src/components/layouts/header.layout.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="bg-brand py-2">
-      <div>Header</div>
+      
+      <div className="container flex items-center justify-between">
+        
+        <div id="logo-wrapper">
+          <a href="#" aria-label="Go to Booking.com">
+            <img width={144} height={24} src="/assets/booking.com.svg" alt="Logo of Booking.com"/>
+          </a>
+        </div>
+
+        <div id="auth">
+        </div>
+
+      </div>
+
     </header>
   )
 }
