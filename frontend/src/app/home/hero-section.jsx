0b553322d109e303b4acea81a3a1e6bf3979f772
AakashJha04 <aakashwillcode@gmail.com>
2026-07-10 22:41:46 +0530

    Updated hero img

--- frontend/src/app/home/hero-section.jsx
+++ frontend/src/app/home/hero-section.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <section className="relative min-h-[372px] bg-black">
-      <img className="absolute size-full z-[1] max-w-[1440px] mx-auto object-cover inset-0" src="./assets/hero-image-1440.jpeg" alt="Booking.com Hero Section" />
+      <img className="absolute size-full z-[1] max-w-[1440px] mx-auto object-cover inset-0" src="./assets/hero-image-1440.png" alt="Booking.com Hero Section" />
       <div className="absolute inset-0 z-[1] max-w-[1440px] mx-auto bg-gradient-to-t from-black/70 via-black/20 to-transparent" />
       <div className="z-[2] relative container py-2">
         <h1 className="mt-4 text-4xl font-extrabold leading-tight text-white drop-shadow-md md:text-5xl">Welcome to <br/> Our Travel Site</h1>
